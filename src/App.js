6651168318a1c5a24577f5edaca1832cf471fd2d
FernandaSGomes <nandasthefany@hotmail.com>
2023-11-09 18:56:24 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header">
+          Hey 👋
+          <a href="/" className="btn btn-primary">
+            Hello
+          </a>
+        </header>
       </div>
     </div>
   );
