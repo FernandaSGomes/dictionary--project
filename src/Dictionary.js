91443f77378ce76c102c0ca15ed3a57fc8bc9974
FernandaSGomes <nandasthefany@hotmail.com>
2023-11-15 18:43:00 +0000

    Styled the application

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,39 +3,58 @@ import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword); //<= ("inside here") I can put to search  a default word i.e. heaven
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     //documentation:https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <label>What word do You want search?</label>
-        <br />
-        <input
-          type="search"
-          placeholder="Type a word"
-          autofocus={true}
-          onChange={handleKeywordChange}
-        />
-      </form>
-      <small>i.e. guarana, passion fruit, heaven, coding</small>
-      <Results results={results} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <label>What word do You want search?</label>
+            <br />
+            <input
+              type="search"
+              placeholder="Type a word"
+              autofocus={true}
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword} // <=defaultValue will show the searching default word
+            />
+          </form>
+          <div className="hint">
+            i.e. guarana, passion fruit, heaven, coding
+          </div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
